ad77c241815612cece0bf24958e7189329f68527
NathanaelGracedo <88015737+NathanaelGracedo@users.noreply.github.com>
2021-09-09 12:39:15 +0700

    Add files via upload

---
+++ perpustakaan1/index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Bootstrap Example</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+</head>
+<body >
+
+<nav class="navbar navbar-expand-sm bg-dark navbar-dark">
+  <ul class="navbar-nav">
+    <li class="nav-item">
+      <a class="nav-link" href="index.php">Home</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="tampil_siswa.php">Data Siswa</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="tampil_kelas.php">Data Kelas</a>
+    </li>
+  </ul>
+</nav>
+<br>
+
+<div class="container-fluid">
+  <center><h3><font color ="152D35" size="50"> Perpustakaan Mastrip </font> </h3></center>
+  <center><img src="mastrip.png"></center>
+</div>
+
+</body>
+</html>
